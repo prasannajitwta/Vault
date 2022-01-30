a2bb35da23efe90c43ce0897d809ad5594793dc8
Prasanna R <82939539+prasannajitwta@users.noreply.github.com>
2022-01-30 10:16:05 +0530

    Add files via upload

---
+++ KNNlab8/KNNlab8.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+from sklearn import datasets
+
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+#dataset = pd.read_csv('iris.csv')
+#feature_columns = ['sepal_length', 'sepal_width', 'petal_length','petal_width']
+#X = dataset[feature_columns].values
+#y = dataset['species'].values
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors = 3)
+classifier.fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
+print("y_pred y_test")
+
+for i in range(len(y_pred)):
+    print(y_pred[i], " ", y_test[i])
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix:")
+print(cm)
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(y_test, y_pred)*100
+print('Accuracy of our model is equal ' + str(round(accuracy, 2)) + ' %.')
